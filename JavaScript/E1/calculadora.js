f57b07f0d9c3a0455e1929de286b13b86a3fe6c2
leonardoxavier11 <104578415+leonardoxavier11@users.noreply.github.com>
2022-10-11 22:31:14 -0300

    comentários

--- JavaScript/E1/calculadora.js
+++ JavaScript/E1/calculadora.js
@@ -1,3 +1,4 @@
+// O let serve para salvar uma variável, com a nomenclatura escolhida e o prompt serve exibir uma caixa de input no navegador para o usuário resposder alguma pergunta.
 
 let input1 = prompt("Favor inserir o primeiro valor numérico");
 confirm("Você inseriu o numero: " + input1)
@@ -5,6 +6,7 @@ confirm("Você inseriu o numero: " + input1)
 let input2 = prompt("favor inserir o segundo valor numérico");
 confirm("Você inseriu o numero: " + input2)
 
+// parseFloat serve para passar uma string para float
 
 let a = parseFloat(input1)
 let b = parseFloat(input2)
@@ -15,6 +17,7 @@ let subtracao = a - b
 let divisao = a / b
 let multiplicacao = a * b
 
+// alert serve para exibir os resultados abaixo em formado de alerta
 
 alert(
     "Resultados: \n" +
