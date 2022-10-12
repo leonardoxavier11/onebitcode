a5667b5d5e2e210f6dbade1e74ac79eb1c7346b9
leonardoxavier11 <104578415+leonardoxavier11@users.noreply.github.com>
2022-10-12 09:14:24 -0300

    Escopo dos Exerc

---
+++ JavaScript/E2/testeDeVelocidade.js
@@ -0,0 +1,2 @@
+// Escreva um programa em JavaScript que permita inserir o nome e a velocidade de dois veículos e exiba na tela uma mensagem dizendo <code>qual dos dois é mais rápido (ou que as valocidades são iguais se este for o caso)
+
