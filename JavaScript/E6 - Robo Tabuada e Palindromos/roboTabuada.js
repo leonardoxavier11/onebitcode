2514f957d37dc7bd8d071f73ee253c3294582422
leonardoxavier11 <104578415+leonardoxavier11@users.noreply.github.com>
2022-10-31 22:34:34 -0300

    roboTabuada

---
+++ JavaScript/E6 - Robo Tabuada e Palindromos/roboTabuada.js
@@ -0,0 +1,11 @@
+// Escreva um programa em javascript que seja capaz de calcular a tabuada de um determinado número (de 1 a 20). O programa deve começar pedindo o número a ser usado nos cálculos e então salvar todos os resultados de multiplicações de 1 a 20 usando o número informado dentro de uma variável do tipo string. Depois o programa deve exibir esses resultados e finalizar.
+
+//VAR
+let n = 0;
+
+n = prompt("Insira um numero:");
+n = parseFloat(n);
+
+for (let i = 1; i <=20; i++) {
+    console.log(n + " X " + i + " = " + n * i);
+}
