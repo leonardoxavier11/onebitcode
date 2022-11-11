b358df35dfd5bfaa95e28caaaf3c8d3d2949e76f
leonardoxavier11 <104578415+leonardoxavier11@users.noreply.github.com>
2022-11-11 12:04:40 -0300

    Aula 41/42 - Parametros

---
+++ JavaScript/Aula 41 - Funcoes/funcoes.js
@@ -0,0 +1,12 @@
+//Estrutura de uma função, sem definição de entrada e nem de saída 
+function ola() {
+    console.log("Olá Mundo!");
+}
+
+//Executando a função
+ola()
+
+//Pode ser executada várias vezes
+ola()
+ola()
+ola()
--- JavaScript/E8 - Cadastro de Imóveis/index.js
+++ JavaScript/E8 - Cadastro de Imóveis/index.js
@@ -17,11 +17,11 @@ let opcao = "";
 
 do {
     //Menu interativo
-    opcao = prompt("Bem vindo!\n\n" +
-        "Atualmente você tem " + imoveisCadastrados.length + " imóveis cadastrados. " + "Por favor, escolha uma das opções.\n\n" +
-        "1 - Cadastrar novo imóvel\n" +
-        "2 - Mostrar todos os imóveis cadastrados\n" +
-        "3 - Sair"
+    opcao = prompt("Bem vindo!" +
+        "\n\nAtualmente você tem " + imoveisCadastrados.length + " imóveis cadastrados. " + "Por favor, escolha uma das opções." +
+        "\n\n1 - Cadastrar novo imóvel" +
+        "\n2 - Mostrar todos os imóveis cadastrados" +
+        "\n3 - Sair"
     );
 
     switch (opcao) {
@@ -36,11 +36,25 @@ do {
             novoImovel.qntBanheiros = prompt("Quantos banheiros há no imóvel??");
             novoImovel.garagem = prompt("Possui garagem? (Sim/Não)");
 
-            //Adicionar o objeto ao final do Array com a função "push"
-            imoveisCadastrados.push(novoImovel);
-
             //Msg de variável cadastrada
-            alert(novoImovel.identificacao + " cadastrado com sucesso");
+            const confirmacao = confirm(
+                "Salvar este imóvel?" +
+                "\n\nIdentificação: " + novoImovel.identificacao +
+                "\nProprietário: " + novoImovel.proprietario +
+                "\nQuartos: " + novoImovel.qntQuatos +
+                "\nBanheiros: " + novoImovel.qntBanheiros +
+                "\nPossui Garagem? " + novoImovel.garagem
+            );
+
+            //Aqui estamos tratando a variável "confirmacao" como true (Se confirmacao for "true", então faça)
+            if (confirmacao) {
+                //Adicionar o objeto ao final do Array com a função "push"
+                imoveisCadastrados.push(novoImovel);
+                alert(novoImovel.identificacao + " cadastrado com sucesso");
+            } else {
+                alert("Imóvel não foi cadastrado")
+            }
+
             break
 
         case "2":
@@ -62,11 +76,11 @@ do {
 
                 //Aqui usamos for para pecorrer pelos indices do array e retornar todas as propriedades de cada objeto que está naquele indice (i)
                 for (i = 0; i < imoveisCadastrados.length; i++) {
-                    alert("Identificação: " + imoveisCadastrados[i].identificacao + "\n\n" +
-                        "Proprietário: " + imoveisCadastrados[i].proprietario + "\n" +
-                        "Quantidade de Quartos: " + imoveisCadastrados[i].qntQuatos + "\n" +
-                        "Quantidade de Banheiros: " + imoveisCadastrados[i].qntBanheiros + "\n" +
-                        "Possui Garagem?: " + imoveisCadastrados[i].garagem + "\n")
+                    alert("Identificação: " + imoveisCadastrados[i].identificacao +
+                        "\n\nProprietário: " + imoveisCadastrados[i].proprietario +
+                        "\nQuantidade de Quartos: " + imoveisCadastrados[i].qntQuatos +
+                        "\nQuantidade de Banheiros: " + imoveisCadastrados[i].qntBanheiros +
+                        "\nPossui Garagem? " + imoveisCadastrados[i].garagem)
                 }
             } else {
                 alert("Ok, retornando ao menu inicial")
