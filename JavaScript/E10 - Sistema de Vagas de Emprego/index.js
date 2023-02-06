6be4045aade3a11649d3e3966d47ef0d4382c319
leonardoxavier11 <104578415+leonardoxavier11@users.noreply.github.com>
2023-02-05 22:44:41 -0300

    Cases: 1, 2 e 4 do Exercício

--- JavaScript/E10 - Sistema de Vagas de Emprego/index.js
+++ JavaScript/E10 - Sistema de Vagas de Emprego/index.js
@@ -1,23 +1,16 @@
-// Escreva um programa em javascript que simule um sistema de vagas de emprego, onde é possível gerenciar as vagas e adicionar candidatos às vagas. Ele deve atender aos seguintes requisitos:
 
-// - Ter um um menu onde é possível escolher entre as diferentes funcionalidades do sistema
-//     - Listar vagas disponíveis
-//     - Criar um nova vaga
-//     - Visualizar uma vaga
-//     - Inscrever um candidato em uma vaga
-//     - Excluir uma vaga
-//     - Sair
-// - A opção de listar as vagas deve mostrar o índice, o nome e a quantidade de candidatos inscritos de todas as vagas.
-// - A opção de criar uma nova vaga deve pedir um nome para a vaga, uma descrição e uma data limite, e também deve pedir que o usuário confirme as informações antes de salvá-las.
 // - A opção de visualizar uma vaga deve pedir o índice da vaga e mostrar todas as informações dela: índice, nome, descrição, data limite, quantidade de candidatos e o nome dos candidatos.
-// - A opção de inscrever um candidato em uma vaga de pedir o nome do candidato, o índice da vaga e então uma confirmação exibindo as informações da vaga antes de salvar o candidato na vaga.
+
 // - A opção de excluir uma vaga deve pedir o índice da vaga, mostrar suas informações e pedir que o usuário confirme a exclusão da vaga antes de realmente exclui-la.
 
 // Este é o exercício de revisão do módulo, então aproveite para utilizar todos os recursos vistos até agora sempre que possível, como os objetos, arrays e funções.
 // ------------------------------//-----------------------------
 
-//Cadastro o Array vazio
+//Funções
+
+//Cadastros dos Arrays vazios
 const vagas = [];
+const nomeDosCandidatos = [];
 
 //Opcao do "switch"
 let opcao = "";
@@ -30,12 +23,31 @@ do {
         "\n3 - Visualizar uma vaga" +
         "\n4 - Inscrever um candidato em uma vaga" +
         "\n5 - Excluir uma vaga" +
-        "\n6 - Sair");
+        "\n6 - Sair"
+    );
 
     switch (opcao) {
-        case "1": alert("Atualmente há " + vagas.length + " disponíveis");
+
+        case "1":
+            alert("Atualmente há " + vagas.length + " disponíveis");
+            //Variável responsável por armazenar as informações contidas no "for" abaixo. Será zerada toda vez que a opeção "1" for acionada
+            let vagasCadastradas = "";
+
+            //"For" responspavel por armazenar as informações citadas abaixo, de todas as vagas, na variável criada acima
             if (vagas.length > 0) {
-                for (let i = 0; i < vagas.length; i++) {
+                if (nomeDosCandidatos.length > 0) {
+                    for (let i = 0; i < vagas.length; i++) {
+                        vagasCadastradas += "\nIndice: " + i +
+                            "\nNome da Vaga: " + vagas[i].nome +
+                            "\nQuantidade de Candidatos: " + vagas[i].nomeDosCandidatos.length + "\n\n"
+                    }
+                } else {
+                    for (let i = 0; i < vagas.length; i++) {
+                        vagasCadastradas += "\nIndice: " + i +
+                            "\nNome da Vaga: " + vagas[i].nome +
+                            "\nQuantidade de Candidatos: 0"
+                    }
+                    alert(vagasCadastradas);
                 }
             }
 
@@ -68,8 +80,63 @@ do {
 
             break
         case "3":
+
             break
         case "4":
+            let nomeCandidato = prompt("Qual o nome do candidato?");
+            let indiceVaga = parseInt(prompt("Qual o indice da vaga que você quer atrelar esse condidato?"));
+
+            //O indice digitado existe? Ou seja, o numero do indice é menor ou igual ao tamanho do array "vagas" ?
+            if (indiceVaga <= vagas.length) {
+
+                //Se sim, existe o array "NomedosCandidato" dentro do objeto que está no indice digitado ?
+                if (vagas[indiceVaga].nomeDosCandidatos) {
+
+                    //Se sim, então adiciona ao array um novo nome do candidato, após confirmação
+                    const confirmacao = confirm(
+                        "Por favor, confirme as informações para atrelar o candidato à vaga" +
+                        "\n\nCandidato: " + nomeCandidato +
+                        "\n\nNome: " + vagas[indiceVaga].nome +
+                        "\nDecrição: " + vagas[indiceVaga].descricao +
+                        "\nData Limite: " + vagas[indiceVaga].dataLimite
+                    );
+
+                    //Aqui estamos tratando a variável "confirmacao" como true (Se confirmacao for "true", então faça)
+                    if (confirmacao) {
+                        //Adicionar o objeto ao final do Array com a função "push"
+                        vagas[indiceVaga].nomeDosCandidatos.push(nomeCandidato)
+                        alert(nomeCandidato + " cadastrado com sucesso");
+                    } else {
+                        alert("Candidato não foi atrelado")
+                    }
+
+                    //Se não, cria o array dentro do objeto e adiciona o nome do candidato
+                } else {
+                    vagas[indiceVaga].nomeDosCandidatos = [];
+                    vagas[indiceVaga].nomeDosCandidatos.push(nomeCandidato);
+
+                    const confirmacao = confirm(
+                        "Por favor, confirme as informações para atrelar o candidato à vaga" +
+                        "\n\nCandidato: " + nomeCandidato +
+                        "\n\nNome: " + vagas[indiceVaga].nome +
+                        "\nDecrição: " + vagas[indiceVaga].descricao +
+                        "\nData Limite: " + vagas[indiceVaga].dataLimite
+                    );
+
+                    //Aqui estamos tratando a variável "confirmacao" como true (Se confirmacao for "true", então faça)
+                    if (confirmacao) {
+                        //Adicionar o objeto ao final do Array com a função "push"
+                        vagas[indiceVaga].nomeDosCandidatos = [];
+                        vagas[indiceVaga].nomeDosCandidatos.push(nomeCandidato);
+                        alert(nomeCandidato + " cadastrado com sucesso");
+                    } else {
+                        alert("Candidato não foi atrelado")
+                    }
+                }
+            } else {
+                alert("Não há nenhuma vaga cadastrada para o indice digitado");
+            }
+
             break
         case "5":
             break
